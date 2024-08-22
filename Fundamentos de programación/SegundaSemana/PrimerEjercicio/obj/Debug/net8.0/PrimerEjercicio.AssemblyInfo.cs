1b939505e66468c173b4d25cad73f6b107b5ed6e
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-22 17:09:15 -0500

    segundo ejercicio / segunda forma de resolver - c#

--- Fundamentos de programación/SegundaSemana/PrimerEjercicio/obj/Debug/net8.0/PrimerEjercicio.AssemblyInfo.cs
+++ Fundamentos de programación/SegundaSemana/PrimerEjercicio/obj/Debug/net8.0/PrimerEjercicio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerEjercicio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40051e8b2e9087a675e4e62a1c90b0af52602492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c123b2b73a60d982e25397886a193adf08f15ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerEjercicio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerEjercicio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f62e435c8f43447d0ce0d7cc1c0a16a3bf0f57e6
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-23 21:26:47 -0500

    segundo  trabajo colaborativo - c#

---
+++ Fundamentos de programación/SegundaSemana/TrabajoColaborativoDos/TrabajoDos.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+2. Una tienda de ropa se encuentra en liquidación y está ofreciendo un descuento del 60%. Elaborar el algoritmo que permita ingresar el precio del producto y que nos muestre cuanto se debe de pagar
+*/
+
+double precioDelProducto, total, descuento = 0.60;
+
+Console.Write("Ingresa el precio del producto: ");
+precioDelProducto = Convert.ToDouble(Console.ReadLine());
+
+total = precioDelProducto * descuento;
+
+Console.WriteLine($"Tienes 60% en descuento que es: {total} dolares.");
+Console.WriteLine($"Total a pagar es de: {precioDelProducto - total} dolares.");
+
+

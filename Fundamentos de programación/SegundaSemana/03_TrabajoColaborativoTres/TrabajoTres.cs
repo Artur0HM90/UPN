fcb18d5afb0bba8f02299e998a8f2c47848f40ca
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-23 21:56:34 -0500

    tercer trabajo colaborativo - c#

---
+++ Fundamentos de programación/SegundaSemana/03_TrabajoColaborativoTres/TrabajoTres.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+3. Desarrollar el algoritmo que permita ingresar 2 números y el programa mostrara el resultado de la
+siguiente operación: (x+y)*(x-y)
+*/
+double primerNumero, segundoNumero, resultado;
+
+Console.Write("Ingresa el primer número: ");
+primerNumero = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingresa el segundo número: ");
+segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+resultado = (primerNumero + segundoNumero) * (primerNumero - segundoNumero);
+
+Console.WriteLine($"El resultado es: {resultado}.");

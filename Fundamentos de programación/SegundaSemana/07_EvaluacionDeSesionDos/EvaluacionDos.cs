da546c23837620945f9a350bf35a76a1e2c4fb77
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-24 23:49:50 -0500

    Evaluacion de sesion dos - c#

---
+++ Fundamentos de programación/SegundaSemana/07_EvaluacionDeSesionDos/EvaluacionDos.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+2. Realizar el algoritmo que me permite transformar horas a minutos
+*/
+
+int horas, total, horaMinitos = 60;
+
+
+Console.Write("Ingrese una cifra en horas: ");
+horas = Convert.ToInt32(Console.ReadLine());
+total = horas * horaMinitos;
+if (horas == 1)
+{
+    Console.Write($"{horas} hora a minutos es {total} minutos.");
+}
+else
+{
+    Console.Write($"{horas} horas a minutos es {total} minutos.");
+}
+

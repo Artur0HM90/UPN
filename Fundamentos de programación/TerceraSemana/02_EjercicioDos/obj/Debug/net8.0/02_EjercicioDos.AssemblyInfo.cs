9f7ac34387b02e1a00771a529e74a79cc8474870
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-27 22:53:11 -0500

    Ejercicio uno y dos - c#

--- Fundamentos de programación/TerceraSemana/02_EjercicioDos/obj/Debug/net8.0/02_EjercicioDos.AssemblyInfo.cs
+++ Fundamentos de programación/TerceraSemana/02_EjercicioDos/obj/Debug/net8.0/02_EjercicioDos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02_EjercicioDos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5f5806adea2a86aa8942d388d138e88e69e0c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c782b4295a1a9a1478bb8db652b14cd88450be8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("02_EjercicioDos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02_EjercicioDos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

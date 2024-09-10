1c3fcf345632b20c97b6af3b633a6e413c4eef82
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-09-09 22:10:37 -0500

    Quinta semana - ejercicio 2 - c#

---
+++ Fundamentos de programación/QuintaSemana/02_EjercicoDos/EjercicioDos.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string clave = "1234", password;
+int intetos = 0;
+
+do
+{
+    Console.Write("Ingrese contaseña: ");
+    password = Console.ReadLine();
+    if (password != clave)
+    {
+        Console.WriteLine("Error");
+        intetos++;
+    }
+    else
+    {
+        break;
+    }
+} while (intetos < 3);
+Console.WriteLine("Tu contraseña es correcta. Bienvenido.");
+
+
+
+
+/*  SEGUNDA FORMA 
+//Crear un algoritmo que permita 3 intentos para ingresar a un sistema de información.
+
+string clave = "1234", password;
+int intentos=3;
+
+do
+{
+    Console.WriteLine("Ingrese su contraseña");
+    password = Console.ReadLine();
+    if (password == clave)
+    {
+        Console.WriteLine("Acceso autorizado, bienvenido al sistema");
+    }
+    intentos--;
+    if(intentos == 0)
+    {
+        Console.WriteLine("Intentos agotados");
+    }
+} while ((intentos >0 && intentos <= 3) && password != clave);
+Console.ReadKey();
+*/
--- Fundamentos de programación/QuintaSemana/02_EjercicoDos/obj/Debug/net8.0/02_EjercicoDos.AssemblyInfo.cs
+++ Fundamentos de programación/QuintaSemana/02_EjercicoDos/obj/Debug/net8.0/02_EjercicoDos.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01_EjercicoUno")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02_EjercicoDos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7ca1cdcacadb996794944370db5d7819897d00")]
-[assembly: System.Reflection.AssemblyProductAttribute("01_EjercicoUno")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01_EjercicoUno")]
+[assembly: System.Reflection.AssemblyProductAttribute("02_EjercicoDos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02_EjercicoDos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.

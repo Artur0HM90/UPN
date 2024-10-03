3470f4a6a77a924b6c9318464859095478c9443e
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-10-02 21:56:01 -0500

    PC7 - Segundo Ejercicio terminado

--- Fundamentos de programación/PC7/EJERCICIOS-PC7/02_SegundoEjercicio/obj/Debug/net8.0/02_SegundoEjercicio.AssemblyInfo.cs
+++ Fundamentos de programación/PC7/EJERCICIOS-PC7/02_SegundoEjercicio/obj/Debug/net8.0/02_SegundoEjercicio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02_SegundoEjercicio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f81f61b21062336401e42c37020022e3a07143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19d52db3b7b4a6f1f5b537535269f35c37097c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("02_SegundoEjercicio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02_SegundoEjercicio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

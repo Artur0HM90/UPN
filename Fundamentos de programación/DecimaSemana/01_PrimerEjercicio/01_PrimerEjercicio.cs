c93eddb57aa7460ab15879835867ae6aed1cb7e8
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-10-14 21:47:04 -0500

    Decima semana / primer y segundo ejercicio

---
+++ Fundamentos de programación/DecimaSemana/01_PrimerEjercicio/01_PrimerEjercicio.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int ingreseNumero;
+do
+{
+    Console.Write("Ingrese un número: ");
+    ingreseNumero = Convert.ToInt32(Console.ReadLine());
+    if (ingreseNumero <= 0 || ingreseNumero >= 16)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Debes ingresar un número del 1 al 15.");
+        Console.ResetColor();
+    }
+
+} while (ingreseNumero <= 0 || ingreseNumero >= 16);
+
+for (int i = 1; i < ingreseNumero; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.ForegroundColor = ConsoleColor.Green; // esta linea es para poner el color
+        Console.Write("*");
+        Console.ResetColor(); // esta linea es para que el color se corte ahi
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
+
---
+++ Fundamentos de programación/DecimaSemana/02_SegundoEjercicio/02_SegundoEjercicio.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola Mundo UPN");
+
+
+
+int a, b, tem;
+
+Console.Write("Ingrese el primer numero: ");
+a = Convert.ToInt32(Console.ReadLine()); // 15
+Console.Write("Ingrese eñ segundo numero");
+b = Convert.ToInt32(Console.ReadLine()); // 5
+
+if (a == b)
+{
+    Console.WriteLine("Debe ingresar numeros diferentes");
+}
+else
+{
+    if (a > b)
+    {
+        tem = b; //5
+        b = a; //15
+        a = tem; //5
+    }
+    while (a <= b)
+    {
+        if (a % 2 == 0)
+        {
+            Console.WriteLine(a); //6, 8, ...
+        }
+        a = a + 1;
+    }
+}
+
+Console.ReadKey();

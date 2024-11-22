eab2379f1b18ee5a0b6a456cf4a63a66149f5fbe
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-11-22 17:28:21 -0500

    EF - CuartoEjercicio

--- EF/bandera/obj/Debug/net8.0/bandera.AssemblyInfo.cs
+++ EF/bandera/obj/Debug/net8.0/bandera.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bandera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b09743baa01d7eea620da6ef4e17e30370a9c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0eb3125c7b0856609cf60719c66bf2442fd5a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("bandera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bandera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
